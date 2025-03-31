e7e649e0cce88b2155281929a2e4f94415b6054f
Juan Alves <juanfalvesm@gmail.com>
2025-03-31 19:57:22 -0300

    Mapeamento das rodadas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Square from './App';
+import Game from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Square />
+    <Game />
   </React.StrictMode>
 );
 
