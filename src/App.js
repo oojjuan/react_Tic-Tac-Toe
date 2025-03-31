3ab1b3b14c07f1175f690c377f0337870c74bca4
Juan Alves <juanfalvesm@gmail.com>
2025-03-31 20:28:31 -0300

    Rewind finalizado

--- src/App.js
+++ src/App.js
@@ -56,14 +56,36 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start'
+    }
+    return (
+      <li key={move} >
+        <button onClick={() => jumpTo(move)}> {description} </button>
+      </li>
+    )
+  })
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  
 
   return (
     <div className='game'>
@@ -71,7 +93,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className='game-info'>
-        <ol>{/* TODO */}</ol>
+        <ol> {moves} </ol>
       </div>
     </div>
   )
